866385c9c6578efdeb99e5b4d8ad02746fe4b5df
WaterVapor7 <felicity.roussakies@gmail.com>
2025-02-18 16:45:58 -0800

    Adjusted a few numbers to be able to pick up from observation zone + score. Changed some logic for new intake claw.

--- TeamCode/src/main/java/Common/Robot/IntakeOuttakeV2.java
+++ TeamCode/src/main/java/Common/Robot/IntakeOuttakeV2.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class IntakeOuttakeV2 {
 
-    // Four Bar Servos (Control Hub Ports 2 [FourBarLeft], 3 [FourBarRight], 4[FourBarPitch]a
+    // Four Bar Servos (Control Hub Ports 2 [FourBarLeft], 3 [FourBarRight], 4[FourBarPitch]
     private Servo FourBarLeft;
 
     private Servo FourBarRight;
